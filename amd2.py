aa45c917a061506a791b15b70955b91863814eb2
jcottongin <imcottongin@gmail.com>
2020-02-29 10:38:34 -0500

    changed to selenium

--- amd2.py
+++ amd2.py
@@ -1,155 +1,70 @@
 #!/usr/bin/python3
 #getting different data places in the lists obtained
-
-from lxml import html
-
-import requests
-#import sys   #ascii code fix
-#reload(sys)
-
-#sys.setdefaultencoding('utf-8') #ascii code fix
-page = requests.get('https://www.marketwatch.com/investing/stock/amd')
-tree = html.fromstring(page.content)
-
-#print pages.text
-
-#get get time
-time = tree.xpath('//span[@class="timestamp__time"]')  #create tree of time
-
-times = [time.text_content() for time in tree.xpath('//span[@class="timestamp__time"]')] #get time inside of tree
-#get names 
-
-name = tree.xpath('//h1[@class="company__name"]/text()')  #create tree of name lists
-
-#search for names column makes a list
-
-names = [name.text_content() for name in tree.xpath('//div/h1[@class="company__name"]')]
-##get pricey Average:  48.715
-price = tree.xpath('//bg-quote[@class="value"]/text()')  #create tree of name lists
-
-##get lists of different prices
-openp = tree.xpath('//span[@class="kv__value kv__primary "]/text()')
-openpr = [openp.text_content() for openp in tree.xpath('//span[@class="kv__value kv__primary "]')]
-#print openpr
-print ('AMD Current Stock Prices')
-##get description of different prices
-opendes = tree.xpath('//small[@class="kv__label"]/text()')
-##index = openpr.index('$27.81') #search for list position 
-
-# get low prices 
-daylo = tree.xpath('//span[@class="text low"]/text()')
-daylos = [daylo.text_content() for daylo in tree.xpath('//span[@class="text low"]')]
-print ('Day Low: ',daylos[0]) 
-print ('52 Week Low: ', daylos[1])
-
-#get high prices 
-dayhi = tree.xpath('//span[@class="text high"]/text()')
-#dayhis = [dayhis.text_content() for dayhi in tree.xpath('//span[@class="text high]')]
-print ('Day High: ', dayhi[0])
-print ('52 Week High:', dayhi[1])
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+#open firefox browser
+options=Options()
+options.headless = False
+driver=webdriver.Firefox(options=options)
 
 
+#browser get website
+url = ("https://finance.yahoo.com/quote/AMD?p=AMD&.tsrc=fin-srch") #website
+driver.get(url) #put website inside of firefox
+   
+#afterhours = driver.find_element_by_css_selector("""span.C\(\$primaryColor\)""").text
+#print('After Hours: ,'+afterhours)
 
 
-##compare price
-from decimal import Decimal
-openprice = openpr[0]
-intprice = Decimal(openprice.strip('$')) ##turn open price from zip list to decimal
-currentprice = price[0]  ##put currentprice list in startprice
-currentprice = Decimal(price[0]) ##convert currentprice to decimal
+openpr = driver.find_element_by_css_selector("""div.W\(1\/2\):nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(2) > td:nth-child(2) > span:nth-child(1)""").text  #get open price
 
-daylo = daylos[0]
-daylow = Decimal(daylos[0])
-dayhig = dayhi[0]
-dayhigh = Decimal(dayhi[0])
+print('open price: ' ,openpr) #from open tree get open price
+#index = open.index('48.78') #search for list position 
+#get day range
+dayrange = driver.find_element_by_css_selector("""div.W\(1\/2\):nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(5) > td:nth-child(2)""").text
+##create list of prices in dayrange path
+#dayranges= [dayrange.text_content() for dayrange in tree.xpath('//td[@class="Ta(end) Fw(600) Lh(14px)"]')] 
+print('Day range ',dayrange)
+##index = dayrange.index('46.79 - 48.57') #search for list position 
 
-print ('Current Price: ', currentprice)
-print ('Open Price: ',intprice)
+#print(type(openpr)) #get type of openpr
+#get currentprice
+currentPrice = driver.find_element_by_css_selector(""".Fz\(36px\)""").text
+print ('Current Price: ',currentPrice)
+#get change
+change = driver.find_element_by_css_selector("""span.Trsdu\(0\.3s\):nth-child(2)""").text
+print('Price Change: ',change)
 
-def compareprice():
+##get date and time
+from datetime import datetime
+# datetime object containing current date and time
+now = datetime.now()
 
-	if (currentprice < intprice) :
-		print ('Current Price is ', (currentprice- intprice),'less than open price' )
-	else:
-		print ('Current Price is ', (currentprice-intprice), 'more than open price')
-	return
-compareprice()
-
-def dayAvg(dayhigh,daylow):
-	avg = float(dayhigh + daylow)/2
-	
-
-	print ('Day Average: ',avg)
-	return avg
-avg=dayAvg(dayhigh,daylow)
-
-
-
-#zip diff prices and description
-stock = {}
-for na, pr in zip(opendes, openpr):   #na is name or first column pr is price or 2nd column
-	stock[na] = pr
-
-#print stock
-#zip lists
-import csv
-with open('amd.csv', 'a') as f:  #open amd.csv and export financial summary data
-	for na, pr in stock.items():
-		f.write("%s,*%s\n"%(na, pr))
+print("now =", now)
+# dd/mm/YY H:M:S
+dt_string = now.strftime("%m/%d/%Y %H:%M:%S")
+#print("date and time =", dt_string)	
 
 import csv
-with open('amdprice.csv', 'a') as f:  #open amdprice.csv and write name and price
-	f.write("Current Price: %s * Open Price: %s * Day High:  %s, * Day Low: %s * Day Average: %s * %s\n'"%(currentprice, intprice, dayhi[0] , daylos[0], avg, times))
-	#f.write('Day High:  %s, * Day Low: %s * Day Average: %s\n' %(dayhi[0] , daylos[0], avg))
-##prints same as amdprice except without the strings
-#with open('price.csv', 'a') as f:  #open price.csv and write name and price
-#	f.write("%s *  %s *  %s, *  %s *  %s * %s\n'"%(currentprice, intprice, dayhi[0] , daylos[0], avg, times))
-
-from pandas import DataFrame, read_csv  #read csv file
-import matplotlib.pyplot as plt 
-import pandas as pd ##load panda with pd as the alias
-import numpy as np #use np as numpy
-
-file = r'output.csv'
-df = pd.read_csv(file, sep='*', error_bad_lines=False)
-df = pd.DataFrame(read_csv(file, sep='*', error_bad_lines=False))
-
-
+with open('test.csv', "a+", newline='') as f:
+    f.write("Open price: ,"+openpr + ", Current Price: ,"+currentPrice+ ', Price Change: ,'+change+ ',Day Range: ,'+dayrange+ ',Date: '+dt_string+'\n')
+#
+#from pandas import DataFrame, read_csv
+#import matplotlib.pyplot as plt 
+#import pandas as pd ##load panda with pd as the alias
+#
+#file = r'test.csv'
+#df = pd.read_csv(file, sep=',', error_bad_lines=False)
+#
+#
 #print(df)   ##print file
-df.columns = ['current', 'open', 'high' , 'low' , 'avg', 'date']  #name columns to call 
-#print(df['current'].head())
-def low_high():
-    #print('Low: ',df['low'][0], 'date', df['date'][0])  ##print 'column' and 'row'
-    low = df['low'][0]  #define current from the column and row position to get just the price
-    #print(current.split("Current Price:")) #split off the Current Price
-    ##print(low) #print whats in df['low'][0]
-    low_price = low[10:15]  #print only the price in df['low'][0]
-    print(low_price)
-    #print('date: ',df['date'][0])
-    #print('high: ' ,df['high'][0], df['date'][0])
-    high = df['high'][0]
-    #print(len(high)) # get length of df['high'][0]
-    high_price = (high[12:17])
-    print(high_price)
+#df.columns = ['open', 'price' ,'current' , 'price' ,'low' , 'avg', 'date']  #name columns to call 
+##print(df['current'].head())
+#print('current:',df['current'][0], 'date', df['date'][0])  ##print 'column' and 'row'
+##print('date: ',df['date'][0])
+
+def soffice():    
+    import subprocess
+    subprocess.call(['/usr/bin/soffice', '/root/python/stock/panda/test.csv'])  ##open output with soffice
     return
-#low_high()
-def rewrite():
-    import csv
-    text=open("amdprice.csv", "r")
-    text= ''.join([i for i in text])\
-        .replace("Day Average: ", "")
-    x=open("output.csv", "w")
-    x.writelines(text)
-    x.close()
-rewrite()
-def average():
-    avg = df['avg'][0:]
-    ##avg2=df['avg'].str.strip('Day Average: ') #strip day average
-    #print(avg2)
-    ##df2=pd.DataFrame(avg2)
-    ##df2['avg']=pd.to_numeric(df['avg'], errors='coerce')
-    #print(avg)
-    print("AMD price monthly avg",avg.mean())
-    
-    
-average()
+soffice()
